f64429d02ddeb0e65a9ebd94810a3547589ac2f5
haneoto <39939317+haneoto@users.noreply.github.com>
2025-10-15 17:55:34 +0800

    Update DouKDL-Bot.py

--- DY/DouKDL-Bot.py
+++ DY/DouKDL-Bot.py
@@ -1,4 +1,4 @@
-﻿import subprocess
+import subprocess
 import time
 import sys
 import os
@@ -11,14 +11,13 @@ script_path = r"D:\Tool\DouK-Downloader\Script\DetailData2txt.py"
 def main():
     try:
         print("启动 DouK-Downloader...")
-        # 启动 main.exe，并等待其关闭
         proc = subprocess.Popen([downloader_path])
-        proc.wait()  # 等待下载程序执行完成（用户关闭后继续）
+        proc.wait()
 
         print("检测到 DouK-Downloader 已退出，开始执行 DetailData2txt.py...")
         subprocess.run(["python", script_path], check=True)
 
-        print("任务完成，1.5 秒后退出。")
+        print("任务完成，即将退出。")
         time.sleep(1.5)
 
     except KeyboardInterrupt:
